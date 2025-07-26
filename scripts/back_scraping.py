ccf133af402855d5a37de43916f60f5489e47501
jaredzrks0 <jaredzrks0@gmail.com>
2025-07-26 13:39:59 -0400

    feat(nfl): begin prep for SGO API

--- scripts/back_scraping.py
+++ scripts/back_scraping.py
@@ -3,6 +3,6 @@ from theleague.handlers.nfl_handler import NFLDailyStatsCollector
 
 if __name__ == "__main__":
     collector = NFLDailyStatsCollector(
-        start_date="2012-09-09", cache_frequency=2, caching=True
+        start_date="2012-12-16", end_date="2016-02-18", cache_frequency=10, caching=True
     )
     collector.run()
--- src/theleague/handlers/base_handler.py
+++ src/theleague/handlers/base_handler.py
@@ -6,22 +6,30 @@ from dotenv import load_dotenv
 
 class BaseHandler:
     API_KEY: str
-    daily_allowance: int
 
     def __init__(self):
         # Load in the API Key
         load_dotenv()
-        self.API_KEY = os.getenv("STATPAL_API_KEY")
+        self.API_KEY = os.getenv("SPORTS_GAME_ODDS_API")
 
-    def check_remaining_daily_requests(self, daily_allowance: int = 50000):
-        """Checks the number of requests completed for the day and compares to the allowance"""
+    def check_remaining_daily_requests(self):
+        """Checks the number of requests completed for the month and compares to the allowance"""
 
         # Hit the API to check current daily requests used
-        url = f"https://statpal.io/api/user-request-count/?access_key={self.API_KEY}"
-        response = requests.get(url)
-        total_used_requests = response.json()["request_count"]
+        url = "https://api.sportsgameodds.com/v2/account/usage"
+        headers = {
+        "X-Api-Key": self.API_KEY
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            usage_data = response.json()
+            entity_usage_data = usage_data['data']['rateLimits']['per-month']
+            max_entities, current_entities = entity_usage_data['max-entities'], entity_usage_data['current-entities']
+
 
         # Compare to the user defined daily allowance and
         print(
-            f"Total Used Requests: {total_used_requests:,} --- Total Remaining Requests: {daily_allowance - total_used_requests:,}"
+            f"Total Requested Monthy Entities: {current_entities:,} --- Total Remaining Monthly Entities: {max_entities - current_entities:,}"
         )
